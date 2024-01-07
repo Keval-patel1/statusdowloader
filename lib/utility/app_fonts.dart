7157249645f0a0284b755503e964d1cb452211c1
DELL <poriyabytez@gmail.com>
2024-01-07 16:32:10 +0530

    Select status saver screen

--- lib/utility/app_fonts.dart
+++ lib/utility/app_fonts.dart
@@ -1,11 +1,11 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+class TS {
+  static final fontStyle = _fontStyle();
 
- class TS {
-   static final fontStyle = _fontStyle();
 }
 
 class _fontStyle {
-  static TextStyle _style(double size, Color color,
+   TextStyle _style(double size, Color color,
       {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
     return TextStyle(
       fontFamily: "SF-Pro-Display",
@@ -16,213 +16,123 @@ class _fontStyle {
     );
   }
 
-  TextStyle sfFont2(Color color, {bool isUnderline = false}) {
-    return _style(
-      2,
-      color,
-    );
+  TextStyle sfFont2(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(2, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont4(Color color, {bool isUnderline = false}) {
-    return _style(
-      4,
-      color,
-    );
+  TextStyle sfFont4(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(4, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont6(Color color, {bool isUnderline = false}) {
-    return _style(
-      6,
-      color,
-    );
+  TextStyle sfFont6(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(6, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont8(Color color, {bool isUnderline = false}) {
-    return _style(
-      8,
-      color,
-    );
+  TextStyle sfFont8(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(8, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont10(Color color, {bool isUnderline = false}) {
-    return _style(
-      10,
-      color,
-    );
+  TextStyle sfFont10(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(10, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont12(Color color, {bool isUnderline = false}) {
-    return _style(
-      12,
-      color,
-    );
+  TextStyle sfFont12(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(12, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont14(Color color, {bool isUnderline = false}) {
-    return _style(
-      14,
-      color,
-    );
+  TextStyle sfFont14(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(14, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont16(Color color, {bool isUnderline = false}) {
-    return _style(
-      16,
-      color,
-    );
+  TextStyle sfFont16(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(16, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont18(Color color, {bool isUnderline = false}) {
-    return _style(
-      18,
-      color,
-    );
+  TextStyle sfFont18(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(18, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont20(Color color, {bool isUnderline = false}) {
-    return _style(
-      20,
-      color,
-    );
+  TextStyle sfFont20(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(20, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont22(Color color, {bool isUnderline = false}) {
-    return _style(
-      22,
-      color,
-    );
+  TextStyle sfFont22(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(22, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont24(Color color, {bool isUnderline = false}) {
-    return _style(
-      24,
-      color,
-    );
+  TextStyle sfFont24(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(24, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont26(Color color, {bool isUnderline = false}) {
-    return _style(
-      26,
-      color,
-    );
+  TextStyle sfFont26(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(26, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont28(Color color, {bool isUnderline = false}) {
-    return _style(
-      28,
-      color,
-    );
+  TextStyle sfFont28(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(28, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont30(Color color, {bool isUnderline = false}) {
-    return _style(
-      30,
-      color,
-    );
+  TextStyle sfFont30(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(30, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont32(Color color, {bool isUnderline = false}) {
-    return _style(
-      32,
-      color,
-    );
+  TextStyle sfFont32(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(32, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont34(Color color, {bool isUnderline = false}) {
-    return _style(
-      34,
-      color,
-    );
+  TextStyle sfFont34(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(34, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont36(Color color, {bool isUnderline = false}) {
-    return _style(
-      36,
-      color,
-    );
+  TextStyle sfFont36(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(36, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont38(Color color, {bool isUnderline = false}) {
-    return _style(
-      38,
-      color,
-    );
+  TextStyle sfFont38(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(38, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont40(Color color, {bool isUnderline = false}) {
-    return _style(
-      40,
-      color,
-    );
+  TextStyle sfFont40(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(40, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont42(Color color, {bool isUnderline = false}) {
-    return _style(
-      42,
-      color,
-    );
+  TextStyle sfFont42(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(42, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont44(Color color, {bool isUnderline = false}) {
-    return _style(
-      44,
-      color,
-    );
+  TextStyle sfFont44(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(44, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont46(Color color, {bool isUnderline = false}) {
-    return _style(
-      46,
-      color,
-    );
+  TextStyle sfFont46(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(46, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont48(Color color, {bool isUnderline = false}) {
-    return _style(
-      48,
-      color,
-    );
+  TextStyle sfFont48(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(48, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont50(Color color, {bool isUnderline = false}) {
-    return _style(
-      50,
-      color,
-    );
+  TextStyle sfFont50(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(50, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont52(Color color, {bool isUnderline = false}) {
-    return _style(
-      52,
-      color,
-    );
+  TextStyle sfFont52(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(52, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont54(Color color, {bool isUnderline = false}) {
-    return _style(
-      54,
-      color,
-    );
+  TextStyle sfFont54(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(54, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont56(Color color, {bool isUnderline = false}) {
-    return _style(
-      56,
-      color,
-    );
+  TextStyle sfFont56(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(56, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont58(Color color, {bool isUnderline = false}) {
-    return _style(
-      58,
-      color,
-    );
+  TextStyle sfFont58(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(58, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 
-  TextStyle sfFont60(Color color, {bool isUnderline = false}) {
-    return _style(
-      60,
-      color,
-    );
+  TextStyle sfFont60(Color color, {bool isUnderline = false, FontWeight fontWeight = FontWeight.w300}) {
+    return _style(60, color, isUnderline: isUnderline, fontWeight: fontWeight);
   }
 }
--- lib/utility/app_strings.dart
+++ lib/utility/app_strings.dart
@@ -4,5 +4,11 @@ import 'package:statusdowloader/utility/app_fonts.dart';
 
 class AppString{
   static const String strTermsAndCondition="Terms & Condition";
-
+  static const String strWaStatusSaver="WA status saver";
+  static const String strStatusSaver="Status Saver - Video Download";
+  static const String strInstaAndYouTubeSaver="Insta & youtube saver";
+  static const String strInstaStorySaver="Instagram story saver";
+  static const String strInstaReelSaver="Instagram reel saver";
+  static const String strYoutubeShortVideo="Youtube short videos";
+  static const String strRecoverChat="Recover chat & status";
 }
