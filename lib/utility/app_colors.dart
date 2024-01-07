8b36287fb1755bbdeee3ae4e88d22e58bb7d46fb
DELL <poriyabytez@gmail.com>
2024-01-07 12:47:31 +0530

    Initial commit

---
+++ lib/utility/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color whiteColor=Colors.white;
+}
---
+++ lib/widgets/common_button.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+Widget commonButton({required String str,required }){
+  return Container(
+  height: 160,
+  child: Text(""),
+
+  );
+}
